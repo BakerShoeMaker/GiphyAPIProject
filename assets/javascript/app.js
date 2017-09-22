683cd5bcc859a34c1fd907dd257fb116277d6133
BakerShoeMaker <info@quackenworth.com>
2017-09-22 13:13:25 -0700

    added styling and additional code.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,17 @@
-console.log("Hello World");
 
 
+//#SearchButton is the button ID.
+$("#SearchButton").click( function(){
+
+    console.log("hello world!");
+})
+
+
+
+// //when page is loaded.
+// $(document).ready(function() {
+//     loadPage();
+// });
 
 
 
