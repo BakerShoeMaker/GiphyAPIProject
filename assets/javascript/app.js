775ababc06f8d00e6a2327c97d1d7ffa346c5450
dhollaway <hollaway.david@gmail.com>
2017-09-24 11:27:45 -0700

    added additional logic.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,7 +4,18 @@
 $("#SearchButton").click( function(){
 
     console.log("hello world!");
-})
+    var queryURL = "";
+
+    //make ajax call
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+   }).done(function() {
+        //do something when response comes back.
+        //Add buttons to the screen with the name of the phrase on the button
+        //Add the gif to the div
+    })
+});
 
 
 
