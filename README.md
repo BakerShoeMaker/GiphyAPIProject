dd23fabcf18702c22172d9220e9b35fb957ae33d
dhollaway <hollaway.david@gmail.com>
2017-09-24 20:38:26 -0700

    able to access the gifs from giffy. Having issues displaying on the page.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # GiphyAPIProject
+### Description
 This project allows the user to enter a term in a text box and have the app search that term for a gif via the Giphy API.
+The app requires an Giphy API key in order to access the gifs.
+
+### Technology Used
+* HTML
+* Javascript
+* CSS
+* jQuery
+
+
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,23 +1,56 @@
 
+var topics = ["dogs","lions","wolves","tigers"];
+
+
+function showButtons(){
+    $("#ButtonsGoHere").empty();
+    for(var i = 0; i < topics.length; i++)
+    {
+        console.log("hello world");
+        var dynamicButton = $("<button>");
+        dynamicButton.text(topics[i]);
+        dynamicButton.addClass("buttonProp");
+        $("#ButtonsGoHere").append(dynamicButton);
+    }
+
+}
+
 
 //#SearchButton is the button ID.
 $("#SearchButton").click( function(){
 
-    console.log("hello world!");
-    var queryURL = "";
+    var valueOfSearch = $("#SearchBox").val().trim();
+    topics.push(valueOfSearch);
+    showButtons();
+    console.log(valueOfSearch);
+    // var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=eRJZpx34ZPkPAXT5rQS03wtOJmBQRlvv&rating=q" +valueOfSearch
+    //     +"&limit=10&rating=g";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=eRJZpx34ZPkPAXT5rQS03wtOJmBQRlvv&rating=g&q=" +valueOfSearch
+    +"&limit=10";
 
     //make ajax call
     $.ajax({
         url: queryURL,
         method: 'GET'
-   }).done(function() {
-        //do something when response comes back.
-        //Add buttons to the screen with the name of the phrase on the button
-        //Add the gif to the div
+   }).done(function(response) {
+        var results = response.data;
+        //console.log(results);
+       console.log(results[0].images.fixed_height.url);
+        for(var i = 0; i < results; i++){
+            //$("#GifContainer").text(JSON.stringify(response));
+            //$("#GifContainer").text((response[i].data.images.original_still.url));
+            var gifDiv = $("#GifContainer");
+            var imageURL = results[i].images.fixed_height.url
+            var gifImages = $("img");
+            gifImages.attr("src", imageURL);
+            gifDiv.append(gifImages);
+
+        }
+
     })
 });
 
-
+showButtons();
 
 // //when page is loaded.
 // $(document).ready(function() {
